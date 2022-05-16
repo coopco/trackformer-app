1d7d7214341c387a05f4049f93bd3d1129a02d09
Connor Cooper <cooper.connor.17@gmail.com>
2022-05-16 20:02:41 +1000

    Buttons on download page

--- static/download.js
+++ static/download.js
@@ -1,11 +1,82 @@
 window.onload = () => {
   $('.progress').each((i, obj) => {
     name = obj.id.split('_')[1];
-    update_progress(name)
+    update_progress(name);
   });
+
+  $("#select-all").click(() => {
+    $('.select').prop("checked", true);
+  });
+
+  $("#download-selected").click(() => {
+    // Get names of all selected tasks
+    names = []
+    $('.download').each((i, obj) => {
+      name = obj.id.split('_')[1];
+      if ($('#select_' + name).is(':checked')) {
+        names.push(name)
+      }
+    });
+
+    // Make request to zip all
+    // Download
+  });
+
+  //$("#cancel-selected").click(() => {
+  //  $('.cancel').each((i, obj) => {
+  //    name = obj.id.split('_')[1];
+  //    if ($('#select_' + name).is(':checked')) {
+  //      $(obj).click();
+  //    }
+  //  });
+  //});
+
+  //$(".cancel").click(() => {
+  //  // Get uuid
+  //  // Make cancel request
+  //  // job.cancel()
+  //  // job.delete()
+  //  console.log('asd');
+  //});
+
+  // When the user clicks on the button, open the modal
+    // https://www.w3schools.com/howto/howto_js_copy_clipboard.asp
+    // https://www.w3schools.com/howto/howto_css_modals.asp
+  $("#share-selected").click(() => {
+    $("#myModal").css("display", "block");
+    // TODO If no tasks selected
+    url = window.location.origin + "/u/" + get_selected_uuids().join("-")
+    $("#share-link").val(url);
+  });
+
+  // When the user clicks on <span> (x), close the modal
+  $(".close").click(() => {
+    $("#myModal").css("display", "none");
+  })
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = (e) => {
+    if (e.target == document.getElementById("myModal")) {
+      $("#myModal").css("display", "none");
+    }
+  };
+
+  $("#share-link").click(() => {
+    $("#share-link").select();
+  })
 };
 
-// Code you want to run when processing is finished
+function get_selected_uuids() {
+  names = []
+  $('.select').each((i, obj) => {
+    name = obj.id.split('_')[1];
+    if ($('#select_' + name).is(':checked')) {
+      names.push(name)
+    }
+  });
+  return names
+}
+
 async function update_progress(name) {
   // make request
   console.log(name);
@@ -31,3 +102,7 @@ async function update_progress(name) {
   //$("#link").css("visibility", "visible");
   //$("#download").attr("href", "/uploads/" + name);
 };
+
+async function cancel_task(name) {
+  // make request
+}
